fa65d74577e23fb62c774bf78e73327184ed5d6a
Junior M. Soares <jrmsrs2.0@gmail.com>
2022-06-24 04:45:27 -0300

    Overall update
    update:
    Player obj: weapon sprite
    Main level
    Obj entities
    Enemies: Bat
    UI: Level, TItle screen logo
    
    implements:
    Narrative/cutscenes stuff
    Enemies: Crab
    NPC: mole (cinematic)
    Level: items (upgrades, consumables, drops), checkpoint
    UI: mouse cursor, screen transitions
    
    mechanics
    secondary/ranged attack
    melee combat overall update

---
+++ Consts.gd
@@ -0,0 +1,30 @@
+extends Node
+
+enum { 
+	# Common item states
+	ACTIVE, INACTIVE
+	
+	# Carrot
+	GROUND
+	
+	# Common dynamic object states
+	IDLE, RUN, HURT, FALL, JUMP, 
+	
+	# Very uncommon (or player exclusive)
+	CLIMB, ATTACK, ATTACK2, 
+	
+	# Player Updates
+	DASH, BOOSTJUMP, BOOSTFALL,
+	
+	# AI Ground NPC
+	WALK,
+	
+	# AI Ground Enemy
+	PATROL, ALERT,
+	
+	# AI Flying Enemy
+	CHASE,
+	
+	# CINEMATIC
+	STARTGAME, PRESENTATION, CINIDLE, CINFALL
+}
--- Global.gd
+++ Global.gd
@@ -3,9 +3,22 @@ extends Node
 var reload_enemy: bool = false
 var player_life_max: float = 8
 var player_life = player_life_max
+var checkpoint: Vector2
 var soft_checkpoint: Vector2
 var player_out: bool = false
-var carrot: int = 11
+var carrot: float = 0
+
+var whip_UNLOCKED: bool = false
+var carrot_UNLOCKED: bool = false
+
+var dash_UNLOCKED: bool = false
+var djump_UNLOCKED: bool = false
+# para implementar:
+var excarrot_UNLOCKED: bool = false
+var wjump_UNLOCKED: bool = false
+
+enum { NONE, INSOFT, INHARD, OUT }
+var paused = NONE
 
 func _ready():
 	randomize()
