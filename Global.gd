c661e2aa6cf2b108e1500a2bf42fa308bca40358
Junior M. Soares <jrmsrs2.0@gmail.com>
2022-07-10 04:46:32 -0300

    Overall update
    update:
    Player obj: fix a wallslide bug
    Enemies: item drop rate, individual mechanics (jump, life, etc), etc etc
    Level: fix glitch with some orbs when reset room
    
    implements
    Enemies: Squir + Nut projectile, Crab miniboss, Squir miniboss
    NPC: elevator/movingplatforms rat mechanism
    Level: elevator system, particles at surface
    UI: custom cursor that indicates attack direction
    
    mechanics
    Enemies: ground enemies resets after a while out
    Player: attacks interacts with enemy projectiles (apply force, etc)

--- Global.gd
+++ Global.gd
@@ -1,33 +1,37 @@
 extends Node
 
+var player: Node2D
 var first_position: bool = true
-
-var progress = {
-	"intro1": false,
-	"intro2": false,
-	"presentation1": false,
-	"presentation2": false,
-	"upg": 0,
-}
 
 var reload_enemy: bool = false
 var player_life_max: float = 8
 var player_life = player_life_max
-var checkpoint: Vector2
-var soft_checkpoint: Vector2
-var player_out: bool = false
-var carrot: float = 0
 
-var player: Node2D
+var progress = {
+	"intro1": 			false,
+	"intro2": 			false,
+	"presentation1": 	false,
+	"presentation2": 	false,
+	"rats": 0,
+}
 
 var whip_UNLOCKED: bool = false
 var carrot_UNLOCKED: bool = false
+var carrot: float = 0
 
 var dash_UNLOCKED: bool = false
 var djump_UNLOCKED: bool = false
-# para implementar:
 var excarrot_UNLOCKED: bool = false
 var wjump_UNLOCKED: bool = false
+
+var game_saving: bool = false
+var checkpoint: Vector2
+var soft_checkpoint: Vector2
+var elevator_checkpoint: bool = false
+var elevator_pos: String = "Pos1"
+var elevator_working: bool = false
+
+var player_out: bool = false
 
 enum { NONE, INSOFT, INHARD, OUT }
 var paused = NONE
